d307243fd2c28380995a75a493441254cfb0e91e
Fatih Tuzlu <fatihtuzlu07@gmail.com>
2023-03-16 23:52:06 +0300

    handle request error added for Client class

--- app/Intergration/Client.php
+++ app/Intergration/Client.php
@@ -3,6 +3,7 @@
 namespace App\Integration;
 
 use GuzzleHttp\Client as GuzzleClient;
+use Psr\Http\Message\ResponseInterface;
 
 class Client
 {
@@ -34,7 +35,29 @@ class Client
     public function request(string $method, string $uri, array $options = []): array
     {
         $response = $this->client()->request($method, $uri, $options);
+        $this->handleRequestError($response);
 
         return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function handleRequestError(ResponseInterface $response): void
+    {
+        if ($response->getStatusCode() === 400) {
+            throw new \Exception('Valid data was given but the request has failed.');
+        }
+        if ($response->getStatusCode() === 401) {
+            throw new \Exception('No valid API Key was given.');
+        }
+        if ($response->getStatusCode() === 404) {
+            throw new \Exception('The request resource could not be found.');
+        }
+        if ($response->getStatusCode() === 422) {
+            throw new \Exception('The payload has missing required parameters or invalid data was given.');
+        }
+        if ($response->getStatusCode() === 429) {
+            throw new \Exception('Too many attempts.');
+        }
+
+        throw new \Exception('Something went wrong.');
     }
 }
