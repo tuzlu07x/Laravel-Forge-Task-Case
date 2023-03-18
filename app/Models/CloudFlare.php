b4c496e5e39deb1466c4ea8cebaadc2b4d1b6bb6
Fatih Tuzlu <fatihtuzlu07@gmail.com>
2023-03-18 13:16:39 +0300

    softdelete added.

--- app/Models/CloudFlare.php
+++ app/Models/CloudFlare.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CloudFlare extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = ['domain', 'status', 'name_servers', 'success', 'created_on'];
     protected $dates = ['created_on'];
     protected $casts = ['name_servers' => 'array'];
--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tenant extends Model
 {
-    use HasFactory;
-    protected $fillable = ['username', 'site_id', 'domain', 'aliases', 'directory', 'status', 'deployment_url'];
+    use HasFactory, SoftDeletes;
+    protected $fillable = ['cloudflare_id', 'username', 'site_id', 'domain', 'aliases', 'directory', 'status', 'deployment_url'];
     protected $casts = [
         'aliases' => 'array',
     ];
+
+    public function cloudflare(): BelongsTo
+    {
+        return $this->belongsTo(CloudFlare::class);
+    }
 }
