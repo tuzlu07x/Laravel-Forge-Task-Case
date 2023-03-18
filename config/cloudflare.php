0e28825aaa636d069be68b8e90ddc49251ab66b4
Fatih Tuzlu <fatihtuzlu07@gmail.com>
2023-03-18 13:28:19 +0300

    config added.

---
+++ config/cloudflare.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'url' => env('CLOUDFLARE_URL'),
+    'email' => env('CLOUDFLARE_EMAIL'),
+    'apiKey' => env('CLOUDFLARE_API_KEY'),
+    'accountId' => env('CLOUDFLARE_ACCOUNT_ID'),
+];
