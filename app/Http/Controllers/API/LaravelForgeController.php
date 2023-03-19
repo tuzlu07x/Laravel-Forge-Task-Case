67f0db62e87aef87db4d270dfcbb74867c8a6ed5
Fatih Tuzlu <fatihtuzlu07@gmail.com>
2023-03-19 05:06:02 +0300

    laravel forge controller made clearly

--- app/Http/Controllers/API/LaravelForgeController.php
+++ app/Http/Controllers/API/LaravelForgeController.php
@@ -2,16 +2,9 @@
 
 namespace App\Http\Controllers\API;
 
-use Illuminate\Http\Request;
-use App\Integration\CloudFlare as CloudFlareClass;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TenantRequest;
-use App\Http\Resources\CloudFlareResource;
 use App\Http\Resources\TenantResource;
-use App\Integration\Forge\Forge;
-use App\Integration\Forge\Site;
-use App\Integration\Forge\SSL;
-use App\Models\CloudFlare;
 use App\Models\Tenant;
 
 class LaravelForgeController extends Controller
