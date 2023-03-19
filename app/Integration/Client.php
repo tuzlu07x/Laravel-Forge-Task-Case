c1c8c4412ee867144ec31932ec345478ca5bc01b
Fatih Tuzlu <fatihtuzlu07@gmail.com>
2023-03-19 05:06:35 +0300

    client's try fixed.

--- app/Integration/Client.php
+++ app/Integration/Client.php
@@ -38,7 +38,7 @@ class Client
             $response = $this->client()->request($method, $uri, $options);
             return json_decode($response->getBody()->getContents(), true);
         } catch (RequestException $e) {
-            throw new \Exception($e->getMessage());
+            return json_decode($e->getResponse()->getBody()->getContents(), true);
         }
     }
 }
